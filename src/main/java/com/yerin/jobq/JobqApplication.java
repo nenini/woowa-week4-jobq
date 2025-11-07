1f9c9f9a6abbf4e5a6a896a8a0c5f6d7e4702844
nenini <kyr068600@gmail.com>
2025-11-07 18:09:11 +0900

    chore: 스프링부트 초기 세팅 및 .gitignore

---
+++ src/main/java/com/yerin/jobq/JobqApplication.java
@@ -0,0 +1,13 @@
+package com.yerin.jobq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobqApplication.class, args);
+	}
+
+}
